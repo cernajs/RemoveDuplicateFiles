f4a8d4353675d71b3aa648ca09faa11858c2275e
cernajs <adamcerny13@icloud.com>
2023-12-16 01:28:17 +0100

    working before refactoring

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include <string>
 #include <functional>
-#include "unordered_map"
-#include "set"
+#include <unordered_map>
+#include <set>
+#include <memory>
+
 #include "fstream"
 
 #include "filesystem"
@@ -22,23 +24,33 @@ struct BlankLineSeparationPolicy : SeparationPolicy<T> {
 };
 
 struct Domain {
-    std::string _content;
-    size_t _hash;
+    std::size_t hash;
+    std::string filename;
 
-    Domain(const std::string& content) 
-        : _content(content), _hash(std::hash<std::string>{}(content)) {}
+    Domain() : hash(0) {}
+
+    void addLine(const std::string& line) {
+        std::size_t lineHash = std::hash<std::string>{}(line);
+        hash ^= lineHash;
+        //std::cout << lineHash << std::endl;
+    }
+
+    bool operator==(const Domain& other) const {
+        return hash == other.hash;
+    }
 
     bool operator<(const Domain& other) const {
-        return _hash < other._hash;
+        return hash < other.hash;
     }
 };
+
 
 
 template<typename T, template<typename> class SeparationPolicy>
 class FileProcessor {
   std::string _file_name;
   SeparationPolicy<T> _policy;
-  std::vector<Domain> _domains;
+  std::set<Domain> _domains;
   Domain _current_domain;
 public:
   FileProcessor(const std::string& name) : _file_name(name) {
@@ -52,26 +64,26 @@ public:
 
   void parseLine(const T& line) {
     if(_policy.splitContent(line)) {
-      if (!_current_domain._content.empty()) {
-          _domains.emplace_back(_current_domain);
+      if (_current_domain.hash != 0) {
+          _domains.insert(_current_domain);
           _current_domain = Domain();
         }
     }
     else {
-      _current_domain._content += line;
+      _current_domain.addLine(line);
     }
   }
 
   void finalizeCurrentDomain() {
-    if (!_current_domain._content.empty()) {
-        _domains.emplace_back(_current_domain);
+    if (_current_domain.hash != 0) {
+        _domains.insert(_current_domain);
         _current_domain = Domain();
     }
   }
 
   void clearDomains() noexcept { _domains.clear(); }
 
-  std::vector<Domain> getDomains() const noexcept { return _domains; }
+  std::set<Domain> getDomains() const noexcept { return _domains; }
 
   std::string getFileName() const noexcept { return _file_name; }
 
@@ -111,18 +123,88 @@ struct NoHiddenFilesFilterPolicy : FileFilterPolicy {
     }
 };
 
+struct CompositeFilterPolicy : FileFilterPolicy {
+    std::vector<std::unique_ptr<FileFilterPolicy>> filters;
+
+    CompositeFilterPolicy() = default;
+
+    CompositeFilterPolicy(const CompositeFilterPolicy&) = delete;
+    CompositeFilterPolicy& operator=(const CompositeFilterPolicy&) = delete;
+
+    CompositeFilterPolicy(CompositeFilterPolicy&&) noexcept = default;
+    CompositeFilterPolicy& operator=(CompositeFilterPolicy&&) noexcept = default;
+
+    void addFilter(std::unique_ptr<FileFilterPolicy> filter) {
+        filters.emplace_back(std::move(filter));
+    }
+
+    bool shouldProcessFile(const std::filesystem::path& filePath) override {
+        for (const auto& filter : filters) {
+            if (!filter->shouldProcessFile(filePath)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+class DomainManager {
+  std::unordered_map<std::string, std::set<Domain>> _domains;
+  std::unordered_map<std::size_t, std::string> domainToFile;
+  std::set<Domain> _unique_domains;
+
+public:
+  DomainManager() = default;
+
+  void addDomains(const std::string& fileName, const std::set<Domain>& domains) {
+    _domains[fileName] = domains;
+    for(const auto& domain : domains) {
+      _unique_domains.insert(domain);
+    }
+
+    for (const auto& domain : domains) {
+      domainToFile[domain.hash] = fileName;
+    }
+  }
+
+  void printDomains() const {
+    for(const auto& [file_name, domains] : _domains) {
+      std::cout << file_name << " : " << std::endl;
+      for(const auto& domain : domains) {
+        std::cout << domain.hash << std::endl;
+      }
+    }
+  }
+
+  void printUniqueDomains() const {
+    for(const auto& domain : _unique_domains) {
+      std::cout << domain.hash << "   " << std::endl;
+    }
+  }
+
+  const std::set<Domain>& getUniqueDomains() const noexcept { return _unique_domains; }
+
+  const std::unordered_map<std::size_t, std::string>& getDomainToFile() const noexcept { return domainToFile; }
+
+  const std::unordered_map<std::string, std::set<Domain>>& getDomains() const noexcept { return _domains; }
+};
+
+
 template<typename T, template<typename> class SeparationPolicy, class FileFilterPolicy>
 class FileManager {
   FileProcessor<T, SeparationPolicy> _file;
   FileFilterPolicy _filter;
   std::string _path;
 
-  std::unordered_map<std::string, std::vector<Domain>> _domains;
-  std::set<Domain> _unique_domains;
+  DomainManager _domainManager;
+
+  // std::unordered_map<std::string, std::vector<Domain>> _domains;
+  // std::unordered_map<std::size_t, std::string> domainToFile;
+  // std::set<Domain> _unique_domains;
 public:
-  FileManager(const std::string& path) : _path(path) {
+  FileManager(const std::string& path, FileFilterPolicy filter) : _path(path), _filter(std::move(filter)) {
     _file = FileProcessor<T, SeparationPolicy>();
-    _filter = FileFilterPolicy();
+    _domainManager = DomainManager();
   } 
 
   void parseDomains() {
@@ -140,27 +222,58 @@ public:
 
         auto domains = _file.getDomains();
 
-        _domains[dir.path().filename().string()] = domains;
-        for(const auto& domain : domains) {
-          _unique_domains.insert(domain);
+        _domainManager.addDomains(dir.path().filename().string(), domains);
+        // _domains[dir.path().filename().string()] = domains;
+        // for(const auto& domain : domains) {
+        //   _unique_domains.insert(domain);
+        // }
+
+        // for (const auto& domain : domains) {
+        //   domainToFile[domain.hash] = dir.path().filename().string();
+        // }
+      }
+    }
+  }
+
+  const DomainManager& getDomainManager() const noexcept { return _domainManager; }
+};
+
+class SetCoverFast {
+private:
+  std::unordered_map<std::string, std::vector<std::set<std::size_t>>> file_sets;
+  std::set<std::size_t> universe;
+
+public:
+  SetCoverFast(const std::unordered_map<std::string, std::vector<std::set<std::size_t>>>& file_sets, const std::set<std::size_t>& universe)
+    : file_sets(file_sets), universe(universe) {}
+
+  std::vector<std::string> solve() {
+    std::vector<std::string> cover;
+    while (!universe.empty()) {
+      std::string best_file;
+      std::set<std::size_t> best_file_cover;
+      for (const auto& [file, sets] : file_sets) {
+        std::set<std::size_t> union_set;
+        for (const auto& set : sets) {
+          union_set.insert(set.begin(), set.end());
+        }
+        std::set<std::size_t> intersection;
+        std::set_intersection(union_set.begin(), union_set.end(), universe.begin(), universe.end(),
+                              std::inserter(intersection, intersection.begin()));
+        if (intersection.size() > best_file_cover.size()) {
+          best_file = file;
+          best_file_cover = intersection;
         }
       }
-    }
-  }
-
-  void printDomains() {
-    for(const auto& [file_name, domains] : _domains) {
-      std::cout << file_name << " : " << std::endl;
-      for(const auto& domain : domains) {
-        std::cout << domain._content << std::endl;
+      if (best_file.empty()) {
+        throw std::runtime_error("No valid set cover found");
+      }
+      cover.push_back(best_file);
+      for (std::size_t element : best_file_cover) {
+        universe.erase(element);
       }
     }
-  }
-
-  void printUniqueDomains() {
-    for(const auto& domain : _unique_domains) {
-      std::cout << domain._content << "   " << std::endl;
-    }
+    return cover;
   }
 };
 
@@ -172,9 +285,45 @@ int main(int argc, char* argv[]) {
   }
 
   const std::string& path = args[1];
-  FileManager<std::string, BlankLineSeparationPolicy, NoHiddenFilesFilterPolicy> manager(path);
+
+  CompositeFilterPolicy compositeFilter;
+  //compositeFilter.addFilter(std::make_unique<NameContainsFilterPolicy>(""));
+  compositeFilter.addFilter(std::make_unique<ExtensionFilterPolicy>(".css"));
+  compositeFilter.addFilter(std::make_unique<NoHiddenFilesFilterPolicy>());
+
+  FileManager<std::string, BlankLineSeparationPolicy, CompositeFilterPolicy> 
+    manager(path, std::move(compositeFilter));
   manager.parseDomains();
 
-  manager.printDomains();
+
+  const auto& domains = manager.getDomainManager().getDomains();
+  const auto& unique_domains = manager.getDomainManager().getUniqueDomains();
+  const auto& domainToFile = manager.getDomainManager().getDomainToFile();
+
+  //manager.getDomainManager().printDomains();
+
+  std::unordered_map<std::string, std::vector<std::set<std::size_t>>> file_sets;
+  std::set<std::size_t> universe;
+
+  // Convert domains and unique_domains to file_sets and universe
+  for (const auto& [file, domainSet] : domains) {
+    std::vector<std::set<std::size_t>> sets;
+    for (const auto& domain : domainSet) {
+      sets.push_back({domain.hash});
+    }
+    file_sets[file] = sets;
+  }
+
+  for (const auto& domain : unique_domains) {
+    universe.insert(domain.hash);
+  }
+
+  SetCoverFast setCover(file_sets, universe);
+
+  std::vector<std::string> cover = setCover.solve();
+  for (const auto& file : cover) {
+    std::cout << file << std::endl;
+  }
+  std::cout << std::endl;
   return 0;
 }
